209a54edea77a4cfbdf10b7f3fd5d8c7b5fbbc4b
memento <claude.rc@gmail.com>
2023-01-06 15:03:25 -0600

    Updated a few test for add empty lines

--- refactor/core.py
+++ refactor/core.py
@@ -136,9 +136,8 @@ class Session:
                 raise MaybeOverlappingActions(
                     "When using chained actions, individual actions should not"
                     " overlap with each other."
-                    f"\n   Action attempted: {action}"
+                    f"\n   Action attempted: {action} for node: {ast.unparse(action.node)}"
                     f"\n               Path: {path}"
-                    f"\n      Previous tree: {ast.unparse(previous_tree)}"
                 ) from None
             else:
                 shifts.append((path, stack_effect))
